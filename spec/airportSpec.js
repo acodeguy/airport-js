90b9dba2cb70d498c0dcaccd8187bdbfe87815c1
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-05 14:50:17 +0000

    add failing test for hangar capacity

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,9 +1,13 @@
 describe('Airport', function() {
 
+  var airport;
+
   beforeEach(function() {
-   
+   airport = new Airport(5);
   });
   
-
+  it('has 5 available hangar spaces on creation', function() {
+    expect(airport.hangar.length).toEqual(5);
+  });
 
 });
